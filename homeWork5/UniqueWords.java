76d81e61f788725f43e0976a24413c76d3c5d99b
Ilya-bychyk <69963685+Ilya-bychyk@users.noreply.github.com>
2021-01-18 23:22:39 +0300

    fifth commit

---
+++ homeWork5/UniqueWords.java
@@ -0,0 +1,12 @@
+package homeWork5;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueWords {
+    public static void main(String[] args) {
+        Set<String> uniques = new HashSet<>();
+        String fileName = "D:\\!!!!!!!Курс по JAVA\\Домашки\\Война и мир_книга.txt";
+
+    }
+}
